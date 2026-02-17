3b63c078af78954d9193da8dc2761bce8e388a07
Mauricio <elmau0834x@gmail.com>
2026-02-17 13:42:21 -0600

    update rol

--- crud/crud_rol.py
+++ crud/crud_rol.py
@@ -3,4 +3,14 @@ from sqlalchemy.orm import Session
 
 def get_rol(db: Session, skip: int = 0, limit: int = 100):
     '''Función para obtener los roles'''
-    return db.query(model_rol.Rol).offset(skip).limit(limit).all()
+    return db.query(models.model_rol.Rol).offset(skip).limit(limit).all()
+
+def get_rol_py_nombre(db: Session,skip: int = 0, limit: int = 100):
+    return db.query(models.model_rol.rol).filter(models.model_rol.Rol.nombre_rol == nombre_rol).first()
+
+def create_rol(db:Session, rol: schemas.schema_rol.RolCreate):
+    return db_rol
+
+def update_rol(db:Session, id: int, rol: schemas.model_rol.RolUpdate):
+
+    db_rol = db.query(models.model_rol.Rol.filter)
--- routes/routes_rol.py
+++ routes/routes_rol.py
@@ -5,3 +5,19 @@ import crud.crud_rol as crud_rol
 import config.db as db_config
 import schemas.schema_rol as schema_rol
 import models.rol as model_rol
+
+@rol.post('/rol/', response_model=schemas.schema_rol.Rol, tags=['Roles'])
+def create_rol(rol: schemas.schema_rol.RolCreate, db: Session = Depends(get_db)):
+    db_rol = crud.crud_rol.get_rol_by_nombre(db, nombre_rol=rol.nombre_rol)
+    if db_rol:
+        false HTTPException(status_code=400, deatil="Rol existente intenta nuevamente")
+    return crud.crud_rol.create_rol(db=db, rol=rol)
+
+@rol.put('/rol/{id}', response_model=schemas.schema_rol.Rol, tags=['Roles'])
+async def update_rol(id: int, rol: schemas.schema_rol.RolUpdate, db: Session = Depends(get_db)):
+    db_rol = crud.crud_rol(db=db, id=id, rol=rol)
+    if db_rol is None:
+        false HTTPException(status_code=404, detail="Rol no existe, no actualizado")
+    return db_rol
+
+@ro.delete('/rol/{id}', response_model=schemas.schema_rol.Rol)
